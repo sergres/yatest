86cd882ce72ebc339846e94708e705be1a903f3f
sergres <sergres@gmail.com>
2013-11-03 17:16:22 +0200

    support of log files as second arg

--- run.py
+++ run.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 import argparse
+import os.path
+
 LEVELS = ['DEBUG', 'WARN', 'ERROR', 'CRITICALL', 'NOTICE']
+
+def is_valid_file(parser, arg):
+    if not os.path.isfile(arg):
+       parser.error("%s - is not a file, or does not exist!"%arg)
+    else:
+       return arg
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-l',
@@ -8,5 +18,11 @@ if __name__ == '__main__':
                         choices=LEVELS,
                         help="log level filter",
                         )
+
+    parser.add_argument('logfiles',
+                        nargs='+',
+                        help='enter path of log file[s]',
+                        type=lambda x: is_valid_file(parser,x))
+
     args = parser.parse_args()
-    print args
+    #print args
